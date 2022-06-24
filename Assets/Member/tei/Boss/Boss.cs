caf2ce24834234c727bbb6d4a4bdd7a137c109cb
nemonoa1021 <73728751+nemonoa1021@users.noreply.github.com>
2022-06-24 09:58:51 +0900

    Boss復旧

---
+++ Assets/Member/tei/Boss/Boss.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Move : MonoBehaviour
+{
+    //ボスとプレイヤーの距離管理
+    public GameObject Range_Player;
+    public GameObject Range_Boss;
+
+    //形態変化の時間管理
+    [SerializeField, Header("距離チェッククールタイム")]
+    float Range_Time;
+    private bool Range_Check = true;
+    private float Time_Count = 0;
+
+    //形態変化の距離管理
+    [SerializeField, Header("形態変化距離")]
+    float Range_Change;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間経過でプレイヤーとの距離をチェック
+        if(Range_Check == true)
+        {
+            
+            time_check();
+        }
+    }
+
+    //時間
+    private void time_check()
+    {
+        Time_Count += Time.deltaTime;
+        if (Range_Time <= Time_Count)
+        {
+            Range_Check = false;
+            Time_Count = 0;
+            Range();
+        }
+    }
+
+    //プレイヤーとの距離を計測し形態変化
+    private void Range()
+    {
+        //距離計測
+        Vector2 pos_Player = Range_Player.transform.position;
+        Vector2 pos_Boss = Range_Boss.transform.position;
+        float range_Boss_player = Vector2.Distance(pos_Player, pos_Boss);
+        Debug.Log("距離は" + range_Boss_player);
+        
+        Range_Check = true;
+
+        //弓に形態変化
+        if (range_Boss_player > Range_Change)
+        {
+            Change_Boss_Archer();
+            Debug.Log("アーチャーにフォルムチェンジ！！！");
+        }
+        //剣に形態変化
+        if (range_Boss_player < Range_Change)
+        {
+            Change_Boss_Swordman();
+            Debug.Log("ソードマンにジョブ変更");
+        }
+    }
+
+    //剣士に形態変化
+    private void Change_Boss_Swordman()
+    {
+
+    }
+
+    //弓に形態変化
+    private void Change_Boss_Archer()
+    {
+
+    }
+
+
+}
