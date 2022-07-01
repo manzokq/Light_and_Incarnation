b107a05c2dd9c7cbb55825641dde00789f1482c3
AMO551 <kouta4611@gmail.com>
2022-07-01 13:53:09 +0900

    タイトルUI設定

--- Assets/Member/itou/Scripts/player/Move1.cs
+++ Assets/Member/itou/Scripts/player/Move1.cs
@@ -8,66 +8,79 @@ public class Move1 : MonoBehaviour
     //最初のアップデートの前に呼び出される（一度だけ）
     // Start is called before the first frame update
 
-
-	int num=1;
+    [SerializeField]
+    int num = 1;
     void Start()
     {
         //自身の座標をx方向に+3移動させる
-	this.transform.Translate(new Vector3 (0f,0f,0f));
+        this.transform.Translate(new Vector3(0f, 0f, 0f));
     }
 
     //毎フレーム呼び出される関数
     // Update is called once per frame
     void Update()
     {
-	//自身の座標をx方向に-1移動させる
-	//Time.deltaTimeは前回のフレームからの経過秒数
+        //自身の座標をx方向に-1移動させる
+        //Time.deltaTimeは前回のフレームからの経過秒数
         //this.transform.Translate(new Vector3 (-5f * Time.deltaTime,0f,0f));
-	//↑キーが押された時
-	if(Input.GetKeyDown(KeyCode.UpArrow)&&num!=1)
-	{
-	   this.transform.Translate(new Vector3 (0f,0.45f,0f));
-	   num-=1;
-	}
-	//↓キーが押された時
-	if(Input.GetKeyDown(KeyCode.DownArrow))
-	{
-	   this.transform.Translate(new Vector3 (0f,-0.45f,0f));
-	   num+=1;
-	}
-	if(num!>3)
-	{
-	   this.transform.Translate(new Vector3 (0f,1.35f,0f));
-	   num-=3;
-	}
-    /*if(Input.GetKeyDown(KeyCode.DownArrow)&&num!=3)
-	{
-	   this.transform.Translate(new Vector3 (0f,-0.45f,0f));
-	   num+=1;
-	}*/
-	if(Input.GetKeyDown(KeyCode.Return))
-	{
-		switch(num)
-		{
-			case 1:
-			SceneManager.LoadScene("Test");   
-			break;
-			case 2:
-			SceneManager.LoadScene("Next");  
-			break;
-			case 3:
-			SceneManager.LoadScene("Sinario");
-			break;
+        //↑キーが押された時
+        if (Input.GetKeyDown(KeyCode.UpArrow) && num != 1)
+        {
+            this.transform.Translate(new Vector3(0f, 1f, 0f));
+            num -= 1;
+        }
+        //↓キーが押された時
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.transform.Translate(new Vector3(0f, -1f, 0f));
+            num += 1;
+        }
+        //パット操作
+        //if (Input.GetKeyDown(KeyCode.UpArrow) && num != 1)
+        //{
+        //    this.transform.Translate(new Vector3(0f, 1f, 0f));
+        //    num -= 1;
+        //}
+       
+        //if (Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    this.transform.Translate(new Vector3(0f, -1f, 0f));
+        //    num += 1;
+        //}
+        //
+        if (num! > 3)
+        {
+            this.transform.position = new Vector3(-3f, -1.5f, 0f);
+            num -= 3;
+        }
+        /*if(Input.GetKeyDown(KeyCode.DownArrow)&&num!=3)
+        {
+           this.transform.Translate(new Vector3 (0f,-0.45f,0f));
+           num+=1;
+        }*/
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            switch (num)
+            {
+                case 1:
+                    SceneManager.LoadScene("Test");
+                    break;
+                case 2:
+                    Debug.Log("何もないよ!!");
+                    //SceneManager.LoadScene("Next");
+                    break;
+                case 3:
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                    Application.Quit();
+                #endif
+                    break;
 
-			default:
-			break;
-		}
+                default:
+                    break;
+            }
 
-	}
-    }
-    //1秒間に実行される関数が決まっているUpdate関数
-    void FixedUpdate()
-    {
-	
+        }
     }
 }
