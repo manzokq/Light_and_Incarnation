45d537d867641ee3640db90c7a8565042209f468
Nhelv5388 <20c1020010kf@edu.tech.ac.jp>
2022-07-15 10:06:02 +0900

    no message

--- Assets/Member/kasai/Scripts/Enemy/Slime/Slime.cs
+++ Assets/Member/kasai/Scripts/Enemy/Slime/Slime.cs
@@ -9,34 +9,31 @@ public class Slime : Enemy
     [SerializeField] GameObject _poison;
 
     private float _playerRange;//プレイヤーとの距離
+    //各攻撃の実行する範囲
     [SerializeField] private int _chargeRange = 0;
     [SerializeField] private int _poisonRangeMin = 0;
     [SerializeField] private int _poisonRangeMax = 0;
+
+    //移動処理の切り替え
     [SerializeField] private int _moveSwitchRange = 0;
-
-    
-
-    //弾を保持（プーリング）する空のオブジェクト
-    //Transform poisons;
 
     private GameObject poisonobj = null;    
     private GameObject chargeobj = null;    
 
     private bool inCamera;
 
-    private bool process = false;
-    [SerializeField] private float recastTime=3.0f;
+    private bool process = false;//これがtrueの間は別の処理を実行しない
+    [SerializeField] private float recastTime=3.0f;//攻撃の周期
 
     protected override void Start()
     {
         base.Start();
         playerObject = GameObject.FindWithTag("Player");
-        poisonobj = _poison;//.GetComponent<GameObject>();
-        chargeobj = chargeObject;//.GetComponent<GameObject>();
-        //Debug.Log(chargeobj);
-        //chargeobj.GetComponent<Charge>().atk = Atk1;
-        chargeObject.SetActive(false);
-        poisonobj = Instantiate(_poison);
+        poisonobj = _poison;
+        chargeobj = chargeObject;
+
+        chargeObject.SetActive(false);//攻撃の当たり判定
+        poisonobj = Instantiate(_poison);//攻撃の当たり判定
         //poisonobj.GetComponent<Poison>().atk = Atk1;
         poisonobj.SetActive(false);
 
