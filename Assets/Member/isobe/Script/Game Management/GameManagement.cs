a4b185e7cfd5d62b918cee2db1f55f1849a7536f
sekiRyoma <ryo-ma0110@docomo.ne.jp>
2022-09-01 00:18:07 +0900

    asddf sr

--- Assets/Member/isobe/Script/Game Management/GameManagement.cs
+++ Assets/Member/isobe/Script/Game Management/GameManagement.cs
@@ -46,7 +46,7 @@ public class GameManagement : MonoBehaviour
     [SerializeField]
     public CharacterID Character;
     [SerializeField]
-    public MapID Map;
+    public string Map;
     [SerializeField]
     public AtkID Atk;
 
@@ -69,24 +69,35 @@ public class GameManagement : MonoBehaviour
     }
     private void Start()
     {
-        if (SceneManager.GetActiveScene().name == "PVMap" || SceneManager.GetActiveScene().name == "MapTutorial")
-        {
-            PlayerHP = 100;
-            PlayerMP = 100;
-            PlayerOrb = 100;
-            playerHp=100;
-            playerMp=100;
-            playerOrb=100;
-        }
-        else
-        {
-            PlayerHP = playerHp;
-            PlayerMP = playerMp;
-            PlayerOrb = playerOrb;
-        }
+        if (!(Map == null))
+            if ((Map == "MapTutorial" || Map == "MapOP" || Map == "GameOver") && SceneManager.GetActiveScene().name == "Map1")
+            {
+                PlayerHP = 100;
+                PlayerMP = 100;
+                PlayerOrb = 100;
+                playerHp = 100;
+                playerMp = 100;
+                playerOrb = 100;
+            }
+            else if (SceneManager.GetActiveScene().name == "PVMap" || SceneManager.GetActiveScene().name == "MapTutorial")
+            {
+                PlayerHP = 100;
+                PlayerMP = 100;
+                PlayerOrb = 100;
+                playerHp = 100;
+                playerMp = 100;
+                playerOrb = 100;
+            }
+            else
+            {
+                PlayerHP = playerHp;
+                PlayerMP = playerMp;
+                PlayerOrb = playerOrb;
+            }
 
+        Map = SceneManager.GetActiveScene().name;
         var xbox = GameObject.FindGameObjectWithTag("Player");
-        xboxPlayer =xbox.GetComponent<XboxPlayerContorol>();
+        xboxPlayer = xbox.GetComponent<XboxPlayerContorol>();
         StartCoroutine(GetOrb());
         Application.targetFrameRate = 60;
     }
@@ -129,6 +140,7 @@ public class GameManagement : MonoBehaviour
     }
     public int PlayerAtk(int EnemyHP)　//エネミーにダメージ
     {
+        Debug.Log(EnemyHP);
         switch (PlayerCharacter)
         {
             case CharacterID.Swordsman:
@@ -159,21 +171,9 @@ public class GameManagement : MonoBehaviour
                         break;
                 }
                 break;
-            case CharacterID.Wizard:
-                switch (Atk)
-                {
-                    case AtkID.Atk1:
-                        EnemyHP -= 1;
-                        break;
-                    case AtkID.Atk2:
-                        EnemyHP -= 2;
-                        break;
-                    case AtkID.Atk3:
-                        EnemyHP -= 3;
-                        break;
-                }
-                break;
         }
+        Debug.Log(EnemyHP);
+        Debug.Log("EnemyDame");
         return EnemyHP;
     }
     //MP追加予定
