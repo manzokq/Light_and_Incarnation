26eb9adb65df582f3de74c12187316a123cf36ff
sekiRyoma <ryo-ma0110@docomo.ne.jp>
2022-09-16 22:42:41 +0900

    かいてん

--- Assets/Member/itou/Scripts/Gimikku/KaitenScript.cs
+++ Assets/Member/itou/Scripts/Gimikku/KaitenScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Kaiten : MonoBehaviour
+public class KaitenScript : MonoBehaviour
 {
     /*
     [SerializeField]
