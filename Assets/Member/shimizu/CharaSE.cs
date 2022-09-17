08c79102fbb78d7781886b6e4d1172ba9c1dbea0
ryuta2727 <ryuta7260@icloud.com>
2022-09-17 18:18:52 +0900

    SE(ジャンプ、スライディング、攻撃)

---
+++ Assets/Member/shimizu/CharaSE.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaSE : MonoBehaviour
+{
+    public void SlidingSE()
+    {
+        SEManager.Instance.Sound(SEManager.SoundState.Sound0);
+    }
+    public void JumpSE()
+    {
+        SEManager.Instance.Sound(SEManager.SoundState.Sound1);
+    }
+    public void DodgeSE()
+    {
+        SEManager.Instance.Sound(SEManager.SoundState.Sound2);
+    }
+    public void SwordAtackSE()
+    {
+        SEManager.Instance.Sound(SEManager.SoundState.Sound3);
+    }
+    public void ArrowAtackSE()
+    {
+        SEManager.Instance.Sound(SEManager.SoundState.Sound4);
+    }
+}
